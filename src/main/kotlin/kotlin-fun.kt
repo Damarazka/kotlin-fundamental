6a6ef7a0f03433aa321299f7c35f1e121dbd4b59
askaaaaaaaaaa _ <bolbagantunahan22@gmail.com>
2023-09-13 14:31:00 +0700

    learn how to print and what's the difference of var & val

---
+++ src/main/kotlin/kotlin-fun.kt
@@ -0,0 +1,18 @@
+fun main(){
+    val name = "Damarazka"
+    var school = "IDN"
+    val shs = "Idn boarding school"
+
+    print("Hello my name is ")
+    println(name)
+    println(if (true) "always true" else "always false")
+
+    school = "International development networking"
+    println(school)
+}
+
+// print() = just output the result
+// println() = output will make a new line
+
+// var = it's mutable so the value can be change
+// val = it's immutable so the value can't be change

4556742a334f8eafd4dae267c8bd6650e2833df1
askaaaaaaaaaa _ <bolbagantunahan22@gmail.com>
2023-09-14 10:04:13 +0700

    type data Char

---
+++ src/main/kotlin/KotlinFundamental/char.kt
@@ -0,0 +1,14 @@
+package KotlinFundamental
+
+fun main(){
+    var one : Char = 'A'
+    // val two : Char = 'ABCD' it's incorrect
+
+    println("Vocal " + one++)
+    println("Vocal " + one++)
+    println("Vocal " + one++)
+    println("Vocal " + one--)
+    println("Vocal " + one--)
+    println("Vocal " + one--)
+    println("Vocal " + one--)
+}

f8a71ea19a02077535e2c1820a5c80d2908fbdb7
askaaaaaaaaaa _ <bolbagantunahan22@gmail.com>
2023-09-18 13:57:32 +0700

    add ways to fill nullable

--- src/main/kotlin/KotlinFundamental/nulllable-type.kt
+++ src/main/kotlin/KotlinFundamental/nulllable-type.kt
@@ -1,9 +1,15 @@
 package KotlinFundamental
 
-fun main(){
-    val some : String? = null
+fun main() {
+    val some: String? = null
+    // there is tow method to fill the null value
+    // val fill = some?.length ?:"apaaja"
+    // val fill = some ?: "default value"
+    //println(fill)
+
+
     // val textLength = some.length | it's can't to declare the variable
-    if (some != null){
+    if (some != null) {
         val textLength = some.length
     }
 }

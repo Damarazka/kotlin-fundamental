09b695667d702101a3856ac6af60fcd9df9f70a9
askaaaaaaaaaa _ <bolbagantunahan22@gmail.com>
2023-09-18 13:49:33 +0700

    nullable

---
+++ src/main/kotlin/KotlinFundamental/nulllable-type.kt
@@ -0,0 +1,9 @@
+package KotlinFundamental
+
+fun main(){
+    val some : String? = null
+    // val textLength = some.length | it's can't to declare the variable
+    if (some != null){
+        val textLength = some.length
+    }
+}

942d651663e7b2f1709e51caee66c3a1681a16a5
askaaaaaaaaaa _ <bolbagantunahan22@gmail.com>
2023-09-20 09:36:27 +0700

    expression & statement

---
+++ src/main/kotlin/ControlFlow/expressions-statements.kt
@@ -0,0 +1,12 @@
+package ControlFlow
+
+fun main(){
+    // it's statement
+    val value1 = 2
+    val value2 = 3
+
+    sum(value1,value2)
+}
+
+// it's expression
+fun sum(value1: Int,value2:Int)=value1 + value2
